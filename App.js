dd0b89ccd74af256fa4e89887eef353bf6eae17e
AleksanderUrbanowicz <aleksanderurbanowicz@gmail.com>
2020-06-18 03:21:47 +0200

    testFile.txt, package.json

--- App.js
+++ App.js
@@ -6,7 +6,36 @@ console.log('logger object: '+logger);
 // console.log('App.js module: ');
 // console.log(module);
 // Scoped to that module
+console.log(process.platform);
+console.log(process.env.USER);
+const { EventEmitter } = require ('events');
+const eventEmitter = new EventEmitter();
+eventEmitter.on('log', () => 
+{
+    console.log('on log event')
+})
 
+eventEmitter.emit('log');
+
+//IO
+const{readFile, readFileSync} = require('fs');
+let txt= readFile('testFile.txt','utf8', (err, txt)=>
+{
+    console.log('log after readFile done');
+}
+);
+console.log('log outside readFile');
+
+ txt= readFileSync('testFile.txt','utf8');
+console.log(txt);
+
+//promises
+const{readFile} = require('fs').promises;
+async function hello()
+{
+
+    const file = await readFile('./testFile.txt','utf8');
+}
 // var someName = logger.someString;
 // logger.sayHello(someName);
 // undefined
